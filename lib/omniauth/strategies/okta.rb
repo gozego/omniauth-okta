97db7ff2bcd9f9feca6e5f60f0da98d0222f6ffb
Andrew Patrick Ong Tolentino <atolentino@hawk.iit.edu>
2022-01-13 09:42:26 -0600

    RAP-2699 - Remove quotes

--- lib/omniauth/strategies/okta.rb
+++ lib/omniauth/strategies/okta.rb
@@ -117,7 +117,7 @@ module OmniAuth
       end
 
       def authorize_params
-        if (request.params.has_key?(":idp"))
+        if (request.params.has_key?(:idp))
           super.merge(idp: request.params["idp"])
         else
           super.merge()
